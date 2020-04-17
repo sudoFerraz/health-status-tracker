32ebe15ea4a1f3f12a20e302695918309a1ca36b
Gabriel Ferraz <gabriel@changeinvest.com>
2020-04-17 04:50:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# change-tracker
+This is a simple tracker script that monitors change services health status and bitcoin/ethereum prices on the mac status bar
+
+# Dependencies
+rumps
+https://github.com/jaredks/rumps
+pip install rumps
+
+# Run
+python changeTracker.py
+
+# Run in the background
+## You can always add this into a personal rc on ~/.config to start on initialization
+nohup python changetracker.py &
+
+
+
---
+++ changeTracker.py
@@ -0,0 +1,30 @@
+import rumps
+import urllib
+import json
+
+class AwesomeStatusBarApp(rumps.App):
+    def __init__(self):
+        super(AwesomeStatusBarApp, self).__init__("Awesome App")
+
+    @rumps.timer(60)
+    def changeTracker(self, _):
+        changeUrl = 'https://changeinvest.statuspage.io/api/v2/status.json'
+        coinbaseUrl = 'https://api.coinbase.com/v2/prices/%s/spot'
+        options = ['BTC-USD','ETH-USD']
+        title = ''
+        for option in options:
+            url = coinbaseUrl % option
+            j = json.loads( urllib.urlopen( url ).read() )
+            if (j['data']['base'] == 'BTC'):
+                title = title + u'\u0243' + ' = ' + j['data']['amount'] + ' '
+            else:
+                title = title + j['data']['base'] + ' = ' + j['data']['amount']
+        j = json.loads( urllib.urlopen( changeUrl ).read() )
+        if (j['status']['description'] == 'All Systems Operational'):
+            title = title + '  Change Status: ' + u'\u2713'
+        else:
+            title = title + '  Change Status: SOMETHING IS WRONG'
+        self.title = title
+
+if __name__ == "__main__":
+    AwesomeStatusBarApp().run()
